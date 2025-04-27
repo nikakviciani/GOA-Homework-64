d8b4fefb63072f4e4899544572092fba06e3d81a
nikakviciani <kalichavanika55@gmail.com>
2025-04-27 11:47:52 +0400

    commit

--- day020/homework/task8.py
+++ day020/homework/task8.py
@@ -1,5 +1,4 @@
 # 9) შექმენი სია `numbers = [10, 20, 30, 40, 50]`, შეცვალე მეორე ელემენტი 25-ით და დაბეჭდე განახლებული სია
 numbers = [10, 20, 30, 40, 50]
-print(numbers[1]+25)
-numbers=numbers[0],numbers[1]+25,numbers[2],numbers[3],numbers[4]
+numbers[2] = 20+25
 print(numbers)
---
+++ day022/classwork/task1.py
@@ -0,0 +1,7 @@
+# roca ar vicit siis zoma
+# vwert -1,-2 da bolodan iwyebs atvllas
+
+# 1) შექმენით სია რომელშიც ჩამოწერეთ მინიმუმ 10 პროგრამირების ენას, ის ენა რომელიც ყველაზე ნაკელბად გსურთ რომ შეისწავლოთ ჩაანაცვლეთ ახალი ენით რომლის სწავლასაც ისურვებდით
+p=["pithon","html","css","c","c++","c# ","php","javascript","java","assembler"]
+p[9]="jithon"
+print(p)
