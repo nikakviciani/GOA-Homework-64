aacb4c88642cd48f2f673f49e026d7578ce6e1b4
nikakviciani <kalichavanika55@gmail.com>
2025-04-30 18:57:21 +0400

    commit

---
+++ day024/classwork/task1.py
@@ -0,0 +1,31 @@
+# si
+n=1,5,25,2,6,2,6
+# tu davwert
+print(n[2:])
+# masun ganiutans yvelafers 2idan 
+# fa aseve segvizlia
+n[0:2]="lll","mmmmc/"
+# da oris mbnisvneloba sevcvalpt
+
+# list.reverse() ასეც შეიძლება სიის შემობრუნება
+
+"""```1) შექმენით სია რომელშიც ჩამოწერეთ თქვენთვის სასურველი ელემენტები, მინიმუმ 10```
+**indexing**
+- გამოიტანეთ სიაში არსებული მესამე ელემენტი
+- ჩაანაცვლეთ სიაში არსებული პირველი ელემენტი
+**slicing**
+- დაბეჭდეთ 2-დან მე-5 ელემენტამდე 
+- დაბეჭდეთ მეორე ელემენტიდან ყველა ელემენტი
+- დაბეჭდეთ მეხუთე ელემენტამდე ყვეალფერი
+- დაბეჭდეთ სია შემობრუნებულად"""
+
+
+c=["lamborghini", "audi", "bmw", "mercedes", "toyota", "ford", "honda", "nissan", "chevrolet", "hyundai"]
+print(c[2])
+print(c[0])
+c[0]="porsche"  
+print(c)
+print(c[2:5])
+print(c[1:])
+print(c[:5])
+print(c[::-1])# da aseve seizleba reversed.c
