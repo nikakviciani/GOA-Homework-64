39b000729dd084f4f33316454a10b1e7a39a2dd5
nikakviciani <kalichavanika55@gmail.com>
2026-01-25 23:02:58 +0400

    bat auto commit

---
+++ day098/classwork/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+export default function App() {
+  const style={
+    backgroundColor: 'lightgreen',
+    color: 'darkgreen'
+  }
+  return (
+    // 2) შექმენით რაიმე div 2 სათაურაით: პირველში ეწერება inline styling ხოლო მეორეში object variable styling. პირველ ელემენტს მიანიჭეთ ღია მწვანე ფონის ფერად და მუქი წვანე ტექსტის ფერად inline გასტვილის გზით, ხოლო მეორე ობიექტის გასტილვის გზით.
+    <div>
+        <p style={{ backgroundColor: 'lightgreen', color: 'darkgreen' }}>inline </p>
+        <p style={style}>object variable</p>
+        <p className=''></p>
+    </div>
+  )
+}
